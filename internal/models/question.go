5fe40d6c8d784dd321782029db7267d252f8364a
ggboy <630591905@qq.com>
2025-07-03 21:04:52 +0800

    修复独立 ac 导致的 mysql 插入零值时间戳的报错

--- internal/models/question.go
+++ internal/models/question.go
@@ -40,10 +40,10 @@ type UserQuestionProgress struct {
 	Question   Question `json:"question" gorm:"foreignKey:QuestionID"`
 
 	// 艾宾浩斯相关字段
-	ReviewLevel    int       `json:"review_level" gorm:"default:0"`     // 复习层级 (0-6)
-	LastReviewDate time.Time `json:"last_review_date"`                  // 上次复习时间
-	NextReviewDate time.Time `json:"next_review_date"`                  // 下次复习时间
-	IsCompleted    bool      `json:"is_completed" gorm:"default:false"` // 是否完成学习
+	ReviewLevel    int       `json:"review_level" gorm:"default:0"`        // 复习层级 (0-6)
+	LastReviewDate time.Time `json:"last_review_date"`                     // 上次复习时间
+	NextReviewDate time.Time `json:"next_review_date" gorm:"default:NULL"` // 下次复习时间
+	IsCompleted    bool      `json:"is_completed" gorm:"default:false"`    // 是否完成学习
 
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
